2d2cd0ca0e2ccd5369de890a45066c5e6e9b3ddb
rubenfa <ruben.fernandez.alfonso@gmail.com>
2016-11-22 12:18:55 +0100

    remove unused files

--- lib/blog_samples.ex
+++
@@ -1,2 +0,0 @@
-defmodule BlogSamples do
-end
--- lib/doburgerbehaviour.ex
+++
@@ -1,7 +0,0 @@
-defmodule DoBurgerBehaviour do
-
-  @callback do_burger( ingredients :: term ) ::
-        {:ok, burger :: String.t} | {:error, reason :: String.t }
-
-end
-
