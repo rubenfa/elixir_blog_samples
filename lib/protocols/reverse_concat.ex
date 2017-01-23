ec816dc6d4aebb5f8d413e429ba68fa98c821d93
ruben <contacto@charlascylon.com>
2017-01-23 06:29:00 +0100

    example changes

--- lib/protocols/reverse_concat.ex
+++ lib/protocols/reverse_concat.ex
@@ -1,30 +1,40 @@
 defprotocol ConcatReverse do
-  @doc "Devuelve un string con todos los elementos de la estructura concatenados y al revés "
+  @fallback_to_any true
+
+  @doc "Devuelve un string con todos los elementos de la estructura concatenados y al revés"
   def concat_reverse(data)
 end
 
-defimpl ConcatReverse, for: String do
+defimpl ConcatReverse, for: BitString do
   def concat_reverse(data), do: String.reverse(data)
 end
 
 defimpl ConcatReverse, for: Integer do
   def concat_reverse(data) do
-    data |> to_string |> String.reverse
+    data
+    |> to_string
+    |> String.reverse
   end
 end
 
 defimpl ConcatReverse, for: List do
   def concat_reverse([]), do: ""
   def concat_reverse(data) do
-    data |> Enum.join |> String.reverse
+    data
+    |> Enum.join
+    |> String.reverse
   end
 end
 
 defimpl ConcatReverse, for: Tuple do 
   def concat_reverse(data) do
     data
-    |> Tuple.to_list(data)
+    |> Tuple.to_list
     |> Enum.join
     |> String.reverse
   end
+end
+
+defimpl ConcatReverse, for: Any do
+  def concat_reverse(_), do: ""
 end
